873b01bae1f09f3fac8f7a3635544827478cde34
Loïse M <loise13@hotmail.fr>
2023-10-20 16:52:26 +0200

    ajout de valeurs

--- exo1.sql
+++ exo1.sql
@@ -1,12 +1,37 @@
--- exercice sql numéro 1
-CREATE DATABASES IF NOT EXISTS voyage;
+
+CREATE DATABASE IF NOT EXISTS voyage;
+
+SHOW DATABASES;
 
 USE voyage;
 
-CREATE TABLE IF NOT EXISTS pays(
-    nom VARCHAR(70),
-    capital VARCHAR(70),
-    superficie VARCHAR(70),
-    Habitants VARCHAR(70),
+--DROP TABLE IF EXISTS voyage;
 
-)
+CREATE TABLE IF NOT EXISTS pays (
+    id INT (11) USIGNED AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(70), 
+    capital VARCHAR(70),
+    superficie INT,
+    habitants INT,
+    smic INT,
+);
+
+SHOW TABLES;
+
+DESCRIBE pays;
+
+INSERT INTO pays (id, nom, capital, superficie, habitants, smic)
+VALUES ('1', 'France','paris', 551695, 68042591, 1383 );
+
+INSERT INTO pays (id, nom, capital, superficie, habitants, smic)
+VALUES ('2', 'allemagne','berlin' , 357592, 84358845 , 2080 );
+
+INSERT INTO pays (id, nom, capital, superficie, habitants, smic)
+VALUES ('3', 'belgique', 'bruxelles', 30688, 11697557, 195504 );
+
+INSERT INTO pays (id, nom, capital, superficie, habitants, smic)
+VALUES ('4', 'espagne' ,'barcelone', 506030, 48196693, 11766 );
+
+SELECT * FROM pays;
+
+DROP DATABASE voyage;
