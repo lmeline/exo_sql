4b4e916080040928ae40040ec9a18d2c3fe00047
Loïse M <loise13@hotmail.fr>
2023-10-20 15:21:32 +0200

    ajout d'un nom

--- exo1.sql
+++ exo1.sql
@@ -1,1 +1,12 @@
 -- exercice sql numéro 1
+CREATE DATABASES IF NOT EXISTS voyage;
+
+USE voyage;
+
+CREATE TABLE IF NOT EXISTS pays(
+    nom VARCHAR(70),
+    capital VARCHAR(70),
+    superficie VARCHAR(70),
+    Habitants VARCHAR(70),
+
+)
